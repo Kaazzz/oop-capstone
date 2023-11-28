3a2e5d06010405b6c4047c79c9210af68e013b5e
Kaz <zakfloreta20@gmail.com>
2023-11-28 21:08:49 +0800

    added main

--- GymBro/GymBro.java
+++ GymBro/GymBro.java
@@ -2,7 +2,7 @@ package GymBro;
 
 import javax.swing.*;
 
-public class GymBro {
+public class GymBro extends JFrame{
     private JTextArea taOutputToDo;
     private JTextField tfUser;
     private JButton btnUser;
@@ -13,4 +13,16 @@ public class GymBro {
     private JButton btnExercise;
     private JTextArea taOutputFinished;
     private JLabel lblExercise;
+    private JPanel jpanel;
+
+
+    public static void main(String[] args) {
+        GymBro app = new GymBro();
+        app.setContentPane(app.jpanel);
+        JButton btn = new JButton("lol");
+        app.setSize(500, 300);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setTitle("GymBro App");
+        app.setVisible(true);
+    }
 }
