1d78ae7913288ef44268e67e445657640244a1c3
moriel <morielbien101@gmail.com>
2023-11-28 16:15:07 +0800

    basic layout

--- GymBro/GymBro.java
+++ GymBro/GymBro.java
@@ -1,4 +1,16 @@
 package GymBro;
 
+import javax.swing.*;
+
 public class GymBro {
+    private JTextArea taOutputToDo;
+    private JTextField tfUser;
+    private JButton btnUser;
+    private JComboBox cbxProgram;
+    private JLabel lblUser;
+    private JLabel lblProgram;
+    private JTextField tfExercise;
+    private JButton btnExercise;
+    private JTextArea taOutputFinished;
+    private JLabel lblExercise;
 }
