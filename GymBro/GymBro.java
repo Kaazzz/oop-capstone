fd80b4d8a277d65d9414ba23b2a3bc30cf2983b7
moriel <morielbien101@gmail.com>
2023-12-04 22:28:24 +0800

    changed size

--- GymBro/GymBro.java
+++ GymBro/GymBro.java
@@ -1,30 +1,75 @@
 package GymBro;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class GymBro extends JFrame{
+public class GymBro extends JFrame {
     private JTextField tfUser;
     private JButton btnUser;
-    private JComboBox cbxProgram;
+    private JComboBox<String> cbxProgram;
     private JLabel lblUser;
     private JLabel lblProgram;
     private JPanel jpanel;
-    private JButton addProgramButton;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField numberOfRepsTextField;
-    private JTextField setNumberTextField;
-    private JTextField timerTextField;
-    private JButton finishSetButton;
-    private JButton finishProgramButton;
-    private JTextField exerciseTextField1;
-    private JComboBox comboBox1;
+    private JButton btnAddProgram;
+    private JTextField tfExercise;
+    private JTextArea taExercise;
+    private JTextArea taWelcomeUser;
+    private JButton btnFinishProgram;
+    private JComboBox cbxRPE;
+    private JTextField tfNumberOfSets;
+    private JTextField tfRestTime;
+    private JTextField tfTimer;
+    private JTextField tfSetNumber;
+    private JTextField tfNumberOfReps;
+    private JButton btnFinishSet;
 
+    private ArrayList<String> exercises; // Store exercises for the current program
+
+    public GymBro() {
+        exercises = new ArrayList<>();
+
+        btnUser.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String userName = tfUser.getText();
+                taWelcomeUser.setText("Welcome, " + userName + "!");
+            }
+        });
+
+        btnAddProgram.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String exercise = tfExercise.getText();
+                exercises.add(exercise);
+                updateExerciseTextArea();
+            }
+        });
+
+        // Add more event listeners for other buttons as needed...
+
+        btnFinishProgram.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Implement logic to finish the program and reset data as needed
+                exercises.clear();
+                updateExerciseTextArea();
+            }
+        });
+    }
+
+    private void updateExerciseTextArea() {
+        StringBuilder exerciseText = new StringBuilder();
+        for (String exercise : exercises) {
+            exerciseText.append(exercise).append("\n");
+        }
+        taExercise.setText(exerciseText.toString());
+    }
 
     public static void main(String[] args) {
         GymBro app = new GymBro();
         app.setContentPane(app.jpanel);
-        JButton btn = new JButton("lol");
         app.setSize(800, 500);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setTitle("GymBro App");
